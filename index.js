4d21ec5634327eab5c417050b53ae0ae5b19c975
Enzo Romeiro <enzoromeiro17@gmail.com>
2023-05-22 17:12:35 -0300

    boas práticas e adicionando menu js na página de cadastro

---
+++ index.js
@@ -0,0 +1,9 @@
+function clickMenu() {
+    var menu = document.querySelector ('#burguer2')
+        if ( menu.style.display === 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
+} 
+
