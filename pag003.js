1b864d4f3aa20220aa71928ba63fb6ccc06af8f8
Enzo Romeiro <enzoromeiro17@gmail.com>
2023-05-23 14:01:19 -0300

    mudança de class para id

--- pag003.js
+++ pag003.js
@@ -1,5 +1,5 @@
 function clickMenu() {
-    var menu = document.querySelector (".burguer2")
+    var menu = document.querySelector ("#burguer2")
         if (menu.style.display === "block") {
             menu.style.display = "none";
         } else {
