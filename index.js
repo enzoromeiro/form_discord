0f074fdfdb9c9d67dbe3fc3d79fdf82a2643622d
Enzo Romeiro <enzoromeiro17@gmail.com>
2023-06-01 23:17:32 -0300

    adicionando novas funcionalidades e mudanças

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function clickMenu() {
         }
 } 
 
+
+    function conferir() {
+        let senha = document.getElementById ('senha')
+        let repitaSenha = document.getElementById ('repita_senha')
+
+        if (senha.value !== repitaSenha.value) {
+            repitaSenha.style.backgroundColor = '#f7191973'
+        } else {
+            repitaSenha.style.backgroundColor = '#1f1e2c'
+        }
+    }
+
+
+
